b1a29d92b900b635047153bbdc0809d5e201c3b6
CatDogDwt <84657892+CatDogDwt@users.noreply.github.com>
2021-05-28 22:55:57 +0800

    Update 3.3.c

--- major/C/Code/3.3.c
+++ major/C/Code/3.3.c
@@ -10,7 +10,7 @@ int main(void){
 
 	int GS1,Group,Code,Item,Check;
 
-	printf("输入ISBN(xxx-x-xxx-xxxxx-x):\n");
+	printf("输入ISBN(xxx-x-xxx-xxxxx-x):");
 	scanf("%3d-%1d-%3d-%5d-%1d",&GS1,&Group,&Code,&Item,&Check);
 
 	printf("GS1 prefix:%d\nGroup identifier:%d\nPublisher code:%d\nItem number:%d\nCheck digit:%d\n",GS1,Group,Code,Item,Check);
