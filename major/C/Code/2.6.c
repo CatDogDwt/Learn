62b41d14c2846451ffd51a6d63216d9c38d84b7e
CatDogDwt <2031812980c@gamil.com>
2021-05-28 15:14:12 +0800

    C Language AND Principles Of Personal Computer's code and document

--- major/C/Code/2.6.c
+++ major/C/Code/2.6.c
@@ -8,18 +8,22 @@
 
 int main(void){
 
-	int count;
+	int count,tw,ten,five,one;
 
 	printf("请输入人民币面额：");
 	scanf("%d",&count);
-
-	printf("20元：%d\n",count/20);
-	 
-	printf("10元：%d\n",(count - count/20*20)/10);
-	 
-	printf("5元：%d\n",(count - (count - count/20*20)/10*10 + count/20*20)/5);
 	
-	printf("1元：%d\n",(count - (count - count/20*20)/10*10 + count/20*20)/1);
+	tw = count/20;
+	count -= tw*20;
+	
+	ten = count/10;
+	count -= ten*10;
+	
+	five = count/5;
+	count -= five*5;
+	
+	one = count/1;
+	printf("20元：%d\n10元：%d\n5元：%d\n1元：%d\n",tw,ten,five,one);
 	
 	return 0;
  }
