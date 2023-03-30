ffaaad1c713be2137683482e86cd226ae1a38e57
went <d.went@qq.com>
2023-03-30 19:58:35 +0800

    202303301958

--- FrontEnd/Code/JS/ShangGuiGu/10_DOM/00_DOM练习/01_图片切换/js/imgchange.js
+++ FrontEnd/Code/JS/ShangGuiGu/10_DOM/00_DOM练习/01_图片切换/js/imgchange.js
@@ -1,6 +1,79 @@
-const left = document.getElementsByClassName("ib_left_select");
-const right = document.getElementsByClassName("ib_right_select");
+const left = document.getElementsByClassName("ib_left_select");//上一个
+const right = document.getElementsByClassName("ib_right_select");//下一个
+const bottom = document.getElementsByClassName("ib_bottom_select")[0].children[0].children;//底部li
+const bottomUl = document.getElementsByClassName("ib_bottom_select")
+const img = document.getElementsByTagName("img");//图片集合
+const imgLength = img.length - 1;
+const bottomLength = bottom.length;
+let imgArr = [];
+let imgArrLength = 0;
 
-left.onclick = function () {
-    console.log("54544545")
+//使用getElementsByClassName获取的是类数组 在使用时需注意添加索引
+left[0].onclick = () => {//点击切换上一个
+    change("pervious");
 }
+
+right[0].onclick = () => {//点击切换下一个
+    change("next");
+}
+
+//底部li标签click事件初始化
+(function IiId() {
+    for (let i = 0; i < bottomLength; i++) {
+        bottom[i].setAttribute("id", `${i}`);//绑定id
+        document.getElementById(`${i}`).onclick = () => {//为每个li绑定点击事件
+            img[i].style.zIndex = 1;
+            //存储每次点击的i
+            if(imgArrLength >1){
+                imgArrLength = 0;
+            }
+            imgArr[imgArrLength] = i;
+            imgArrLength++;
+            // console.log(`imgArr[${i}] = ${imgArr[imgArr.length]}`)
+            //使上一个图片的z-index为0
+        }
+    }
+}())
+
+
+const changeClosure = () => {
+    //初始化
+    let i = 0;//设置切换标签
+    bottom[0].className = 'lihover';//底部选项首张背景色
+
+    return (nextOrPervious) => {
+        if (nextOrPervious == "next") {
+            i > imgLength - 1 ? i = 0 : ++i;//0 1 2 3 4 5 6 7 8...
+            //使下一张图片的z-index为1
+            img[i].style.zIndex = 1;
+            //使上一个图片的z-index为0
+            img[i != 0 ? i - 1 : imgLength].style.zIndex = 0;
+            //底部选项样式
+            bottom[i].className = 'lihover';
+            bottom[i != 0 ? i - 1 : imgLength].className = 'linormal';
+
+        } else if (nextOrPervious == "pervious") {
+            i > 0 ? --i : i = imgLength;//7 6 5 4 3 2 1 0 8...
+            //使上一张图片的z-index为1
+            img[i].style.zIndex = 1;
+            //使下一张图片的z-index为0
+            img[i != 8 ? i + 1 : 0].style.zIndex = 0;
+            //底部选项样式
+            bottom[i].className = 'lihover';
+            bottom[i != 8 ? i + 1 : 0].className = 'linormal';
+        } else {
+            console.log("❌");
+        }
+    }
+}
+
+// const bottomLiClosure = (perviousValue) => {
+//     let i = perviousValue;
+//     return () => {
+
+//     }
+// }
+
+// const perviousValue = bottomLiClosure();
+
+const change = changeClosure();
